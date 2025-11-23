ddc2277e1f933357f39ee37bdf346c9743e0f2af
Sebastiaaann <sebastian.almonacid06@inacapmail.cl>
2025-11-23 10:39:58 -0300

    Add React dependency and source map for Vite

---
+++ .vite/deps/chunk-7J7MMCTC.js
@@ -0,0 +1,14 @@
+import {
+  CSSPlugin,
+  gsap
+} from "./chunk-BPNLQR77.js";
+
+// node_modules/gsap/index.js
+var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap;
+var TweenMaxWithCSS = gsapWithCSS.core.Tween;
+
+export {
+  gsapWithCSS,
+  TweenMaxWithCSS
+};
+//# sourceMappingURL=chunk-7J7MMCTC.js.map
